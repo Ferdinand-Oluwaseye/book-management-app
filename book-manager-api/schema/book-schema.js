1a86dadb182204d9546159b54804c2d422fba474
Ferdinand-Oluwaseye <ronferd2012@gmail.com>
2019-08-20 13:27:07 +0100

    finished the schema for book

--- book-manager-api/schema/book-schema.js
+++ book-manager-api/schema/book-schema.js
@@ -1,5 +1,15 @@
 var mongoose = require('mongoose');
 
 exports.BookSchema = new mongoose.Schema({
-    
-})
+    book_name: {
+        type: String
+    },
+    author_name: {
+        type: String
+    },
+    serial_number: {
+        type: Number
+    }
+});
+
+exports.BookModel = mongoose('Book', this.BookSchema);
