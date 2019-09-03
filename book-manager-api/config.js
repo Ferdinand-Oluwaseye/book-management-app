329e186a158354b30efa3f78a27c156c2b55159b
Ferdinand-Oluwaseye <ronferd2012@gmail.com>
2019-09-03 21:00:36 +0000

    modified db url

--- book-manager-api/config.js
+++ book-manager-api/config.js
@@ -3,6 +3,7 @@
  */
 exports.app = {
     PORT: 9000,
-    MONGODB_URI: 'mongodb://' + process.env.MONGO_HOST + ':27017/bookdata',
+   // MONGODB_URI: 'mongodb://' + process.env.MONGO_HOST + '/bookdata',
+    MONGODB_URI: process.env.DATABASE,
     logErrors: true
 };
