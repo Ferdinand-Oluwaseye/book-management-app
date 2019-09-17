dde65964740e3e6a60618d66a95fe5da1d99e568
Ferdinand-Oluwaseye <ronferd2012@gmail.com>
2019-09-17 12:27:29 +0100

    changed the ip to api

--- book-manager-ui/src/app/book.service.ts
+++ book-manager-ui/src/app/book.service.ts
@@ -6,7 +6,9 @@ import { HttpClient } from '@angular/common/http';
 })
 export class BookService {
 
-  uri = 'http://35.234.141.159:9000/book';
+  //uri = 'http://35.234.141.159:9000/book';
+  
+  uri = '/api/'
 
   constructor(private http: HttpClient) { }
 
